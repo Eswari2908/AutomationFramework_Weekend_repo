981c52804d3d47a043331a89d0a362b0ba669f25
Eswari <Eswari@LAPTOP-VC67H06G>
2025-04-05 15:59:45 +0530

    Fork and pull request added

---
+++ AutomationFramework_Weekend/src/test/java/Eswari/TestSample.java
@@ -0,0 +1,11 @@
+package Eswari;
+
+import org.testng.Reporter;
+
+public class TestSample {
+	@org.testng.annotations.Test
+	public void Test() {
+		Reporter.log("Fork and Pull Request added");
+	}
+
+}
